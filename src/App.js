9fc33bbc667db11972d7ebac9b18f0565b236c55
Rohini-Mudpalliwar <149353311+Rohini-Mudpalliwar@users.noreply.github.com>
2024-04-19 20:25:14 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import PlayerDetails from './components/PlayerDetails';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='heading'>Tic Tac Toe</h1>
+      <PlayerDetails/>
+    </div>
+  );
+}
+
+export default App;
